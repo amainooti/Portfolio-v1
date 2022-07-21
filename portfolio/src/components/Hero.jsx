5c47c2ea64cc63f4125f47cf8c6634961615f9ad
amainooti <amainooti@gmail.com>
2022-07-21 16:58:29 +0100

    added images to the hero section

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Hero.css";
 import waving from "../images/Waving Hand Medium Light Skin Tone.png";
+import heroImage from "../images/Hero Image.png"
 
 function Hero() {
   return (
@@ -11,9 +12,16 @@ function Hero() {
                 <img src= {waving} alt="waving hand" />
                 <p>i'm</p>
             </div>
+            <div className="left-intro">
+                <h1>Amaino Oti</h1>
+                <p>Backend web developer specialising in building exceptional and accessible digital experiences.</p>
+                <a href="/">Hire me</a>
+            </div>
         </div>
         <div className="right-side">
-
+            <div className="right-side-img">
+                <img src= {heroImage} alt="Hero" />
+            </div>
         </div>
     </div>
   )
