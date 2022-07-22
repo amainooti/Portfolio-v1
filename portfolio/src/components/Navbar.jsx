44465cdc1a799fe92c309f4c9faf72e7d3b92e32
amainooti <amainooti@gmail.com>
2022-07-22 02:07:39 +0100

    added hover effect on logi

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import {React, useState, useRef} from 'react';
 import logo from "../images/AO logo (1).png";
+import otherLogo from "../images/AO logo (2).png";
 import "./Navbar.css";
 
 
 function Navbar() {
+  const imageRef = useRef(null);
+
   return (
     <div className='container'>
         <nav className="navbar">
             <div className="logo-container">
-                <img src={logo} alt="Logo" />
+                <img
+                onMouseOver = {()=> {
+                  imageRef.current.src = otherLogo
+                }}
+                onMouseOut = {()=> {
+                  imageRef.current.src = logo
+                }}
+                src = {logo}
+                alt="Logo"
+                ref = {imageRef}
+               />
             </div>
             <ul className="nav-items">
                 <li>Home</li>
